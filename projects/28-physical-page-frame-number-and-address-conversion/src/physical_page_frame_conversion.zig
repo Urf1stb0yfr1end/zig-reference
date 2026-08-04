9927860df3f71df2b8e9c4024cd3d6c92467b78e
thanks-cohn <officialmahasbiz@gmail.com>
2026-08-04 06:59:57 -0600

    Add physical page frame conversion module

---
+++ projects/28-physical-page-frame-number-and-address-conversion/src/physical_page_frame_conversion.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+const addresses = @import("../../18-distinct-memory-address-types/src/distinct_memory_address_types.zig");
+
+pub const PageSize = 4096;
+
+pub const PhysicalPageFrameNumber = struct {
+    value: usize,
+
+    pub fn init(value: usize) PhysicalPageFrameNumber {
+        return .{ .value = value };
+    }
+
+    pub fn toAddress(self: PhysicalPageFrameNumber) error{Overflow}!addresses.PhysicalAddress {
+        const raw = std.math.mul(usize, self.value, PageSize) catch return error.Overflow;
+        return addresses.PhysicalAddress.init(raw);
+    }
+
+    pub fn fromAddress(address: addresses.PhysicalAddress) error{Unaligned}!PhysicalPageFrameNumber {
+        if ((address.raw() & (PageSize - 1)) != 0) return error.Unaligned;
+        return .{ .value = address.raw() / PageSize };
+    }
+};
+
+test "page frame numbers and aligned physical addresses round trip" {
+    const frame = PhysicalPageFrameNumber.init(3);
+    const address = try frame.toAddress();
+    try std.testing.expectEqual(@as(usize, 0x3000), address.raw());
+    try std.testing.expectEqual(@as(usize, 3), (try PhysicalPageFrameNumber.fromAddress(address)).value);
+    try std.testing.expectError(error.Unaligned, PhysicalPageFrameNumber.fromAddress(addresses.PhysicalAddress.init(0x3001)));
+}
